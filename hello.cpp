5b0fa78324f9137707b0cc2737ce24cfbe2ac3f5
vinodpescs <125767431+vinodpescs@users.noreply.github.com>
2023-02-19 22:22:23 +0530

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "PES1UG20CS702\n";
+  cout << "Vinod Kumar E\n";
+  cout << "L section\n";
+  return 0;
+}
